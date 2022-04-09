649fb89a97a3e32bb5a2fc0c303dfb3a9d3c4072
saurabh-gaonkar <saurabhg@axioned.com>
2022-04-09 20:17:16 +0530

    js added for mobile screen menus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-/* Author: 
-
-*/
-
-
-
-
+$(document).ready(function(){
+    $(".mobile-header__menu").click(function(){
+        $(".header__top").slideToggle();
+        $(".header__bottom").slideToggle();
+    });
+});
 
 
 
